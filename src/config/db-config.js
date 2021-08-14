baa0246224a4aa7ce6c6aeb213d9e19cba872ef2
sowish23 <soso2266@naver.com>
2021-08-14 16:55:59 +0900

    add user scheme

---
+++ src/config/db-config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const collectName = {
+  User: "User",
+  Board: "Board",
+  Sympathy: "Sympathy",
+};
---
+++ src/repositories/user-repository.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { collectName } from '../config/db-config';
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		// eslint-disable-next-line
+		match: [
+			/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+			"이메일 형식을 지켜주세요.",
+		],
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	nickname: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	isAdmin: {
+		type: Boolean,
+		default: false,
+	},
+},
+{
+	timestamps: true,
+});
+
+const userRepository = mongoose.model(collectName.User, userSchema);
+export default userRepository;
